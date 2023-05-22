8aa569822b7e52cbe722bfad15119fe3b896fd7e
CurtisScott01 <133857832+CurtisScott01@users.noreply.github.com>
2023-05-22 12:18:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,11 +33,11 @@ streamlit.write('Sizes Available: ',df2[2])
 streamlit.write(df2[3])
 
 
-"""
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
-"""
+
